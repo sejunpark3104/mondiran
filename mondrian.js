74913e23de278e645356f04dd797217c3581bef0
SJ_BAK_3104 <wound3004@gmail.com>
2018-11-14 11:10:52 +0900

    Update mondrian.js

--- mondrian.js
+++ mondrian.js
@@ -1,9 +1,4 @@
-// mondrian function generator
-// author: grace guan
-// date: 6/22/17
-//////////////////////////////////////////////////////////////////////////////////
 
-// global variables
 
 var r;
 
@@ -12,23 +7,20 @@ var vL = new Array(numvL + 1);
 var numhL = Math.floor(Math.random(5)) + 4; // min: 4, max: 8
 var hL = new Array(numhL + 1);
 
-// a coordinate (x, y) is filled if the rectangle to its bottom right is
-// also filled.
+
 var indexFilled = new Array(numvL);
 for (var i = 0; i < numvL; i++) {
   indexFilled[i] = new Array(numhL);
 }
 
-//////////////////////////////////////////////////////////////////////////////////
 
-// set the canvas to not repeat, background white
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noLoop();
   background(255);
 }
 
-// draw the mondrian!
+
 function draw() {
   drawBaseLines();
 
@@ -37,7 +29,7 @@ function draw() {
     trials++;
     fillColor();  
 
-    // rect syntax: start point (x, y) (length x, length y). calculate indexes
+   
     var startX = Math.floor(random(numvL - 2));
     var startY = Math.floor(random(numhL - 2));
     var endX = Math.floor(random(numvL - startX - 2)) + startX + 1;
@@ -46,23 +38,21 @@ function draw() {
     var tempStartX = startX;
     var tempStartY = startY;
 
-    // check if these indices are valid (whether or not they have already been filled)
     var filled = false;
     while (startX < endX) {
       startY = tempStartY;
       while (startY < endY) {
         if(indexFilled[startX][startY]) { 
           filled = true; 
-        } // end if statement
+        }
         startY = startY + 1;
-      } // end while start Y < end Y loop
+      }
       startX = startX + 1;
-    } // end while start X < end X loop
+    }
 
     startX = tempStartX;
     startY = tempStartY;
 
-    // if they haven't been filled, fill them and increase the count
     if (! filled) {
       rect(vL[startX], hL[startY],
        vL[endX] - vL[startX], hL[endY] - hL[startY]);
@@ -71,19 +61,15 @@ function draw() {
         while (startY < endY) {
           indexFilled[startX][startY] = true;
           startY = startY + 1;
-        } // end while startY < endY loop
+        } 
         startX = startX + 1;
-      } // end while startX < endX loop
-    } // end if not filled statement
-  } // end while numFilled < 3 block 
+      }
+    } 
+  } 
 }
 
-// draw the lines at least 15 px apart, each to the right/bottom of
-// each other. draws the lines in black and stores them in arrays.
 function drawBaseLines() {
-  strokeWeight(3); // thin-ish lines!
-
-  // draw the vertical lines
+  strokeWeight(3); 
   for (var i = 0; i < numvL; i++) {
     if (i == 0) { r = random(windowWidth / 3); }
     else { r = random(windowWidth / 3) + vL[i - 1] + 15; }
@@ -92,7 +78,6 @@ function drawBaseLines() {
     line(r, 0, r, height);
   }
 
-  // draw the horizontal lines
   for (var i = 0; i < numhL; i++) {
     if (i == 0) { r = random(windowHeight / 3); }
     else { r = random(windowHeight / 3) + hL[i - 1] + 15; }
@@ -102,10 +87,8 @@ function drawBaseLines() {
   }
 }
 
-// fill the color red yellow or blue
 function fillColor() {
   var randomColor = Math.floor(random(3));
-  //console.log(randomColor);
   if (randomColor == 0) {
     fill('#ff0000'); // red
   } else if (randomColor == 1) {
@@ -115,22 +98,19 @@ function fillColor() {
   }
 }
 
-// reset the canvas if mouse is pressed
 function mousePressed() {
   clear();
-  // reset the other numbers
   numvL = Math.floor(Math.random(5)) + 6; // min: 6, max: 10
   vL = new Array(numvL + 1);
   numhL = Math.floor(Math.random(5)) + 4; // min: 4, max: 8
   hL = new Array(numhL + 1);
 
-  // a coordinate (x, y) is filled if the rectangle to its bottom right is
-  // also filled.
+
   indexFilled = new Array(numvL);
   for (var i = 0; i < numvL; i++) {
     indexFilled[i] = new Array(numhL);
   }
-  // reset it to always be true
+  
   var i;
   var j;
   while (i < numvL) {
